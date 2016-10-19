0cfd17418b028a45c2dc957ef766c0326e31f5fa
Tibor Goldschwendt <goldschwendt@cip.ifi.lmu.de>
2016-10-19 23:06:26 +0200

    Added formula for tinydir.

---
+++ tinydir.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Tinydir < Formula
+  desc "Lightweight, portable and easy to integrate C directory and file reader"
+  homepage ""
+  url "https://github.com/cxong/tinydir/archive/ae6501d50b4540b7bd81348216ebe7b02696e5b4.zip"
+  sha256 "41c8bfcb53e62b9b74371358a7b12f4dfa7be773baf87e275f7d838adc38d15c"
+  version "0.20160620.1" # 0.<yyyymmdd>.<commit number that day>
+
+  def install
+  	include.install "tinydir.h"
+    #system "cmake", ".", *std_cmake_args
+    #system "make", "install"
+  end
+end
