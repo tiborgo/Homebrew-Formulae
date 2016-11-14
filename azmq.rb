90288223ade33b227995be9213b8a9e78e73d3aa
Tibor Goldschwendt <tibor.goldschwendt@googlemail.com>
2016-11-14 14:31:06 +0100

    Added test for azmq.

--- azmq.rb
+++ azmq.rb
@@ -15,4 +15,69 @@ class Azmq < Formula
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <azmq/actor.hpp>
+      #include <azmq/util/scope_guard.hpp>
+
+      #include <asio/buffer.hpp>
+
+      #include <array>
+      #include <thread>
+      #include <iostream>
+      #include <assert.h>
+
+      std::array<asio::const_buffer, 2> snd_bufs = {{
+          asio::buffer("A"),
+          asio::buffer("B")
+      }};
+
+      std::string subj(const char* name) {
+          return std::string("inproc://") + name;
+      }
+
+      int main() {
+        asio::error_code ecc;
+        size_t btc = 0;
+
+        asio::error_code ecb;
+        size_t btb = 0;
+        {
+          std::array<char, 2> a;
+          std::array<char, 2> b;
+
+          std::array<asio::mutable_buffer, 2> rcv_bufs = {{
+            asio::buffer(a),
+            asio::buffer(b)
+          }};
+
+          asio::io_service ios;
+          auto s = azmq::actor::spawn(ios, [&](azmq::socket & ss) {
+            ss.async_receive(rcv_bufs, [&](asio::error_code const& ec, size_t bytes_transferred) {
+              ecb = ec;
+              btb = bytes_transferred;
+              ios.stop();
+            });
+            ss.get_io_service().run();
+          });
+
+          s.async_send(snd_bufs, [&] (asio::error_code const& ec, size_t bytes_transferred) {
+            ecc = ec;
+            btc = bytes_transferred;
+          });
+
+          asio::io_service::work w(ios);
+          ios.run();
+        }
+
+        assert(ecc == asio::error_code());
+        assert(btc == 4);
+        assert(ecb == asio::error_code());
+        assert(btb == 4);
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++11", "-lzmq", "-o", "test"
+    system "./test"
+  end
 end
