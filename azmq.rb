d8501643c14183ecc4ea3b86dbd9fff23ff91fa0
Tibor Goldschwendt <tibor.goldschwendt@googlemail.com>
2016-11-14 13:56:28 +0100

    Using custom Azmq without boost.

--- azmq.rb
+++ azmq.rb
@@ -1,14 +1,15 @@
 require 'formula'
 
 class Azmq < Formula
-  desc "C++ language binding library integrating ZeroMQ with Boost Asio"
-  homepage "https://github.com/zeromq/azmq"
-  url "https://github.com/zeromq/azmq/archive/c23221bd572ee3034287d69940cfb102296e5d9a.zip"
-  sha256 "8ce2518276d75a938ca0d6178dcc44831027d1e9f2209f5bb0bcc3f3eb5644bf"
-  version "2016.2.11" # <year>.<month>.<day>
+  desc "C++ language binding library integrating ZeroMQ with Asio"
+  homepage "https://github.com/tiborgo/azmq"
+  url "https://github.com/tiborgo/azmq/archive/dc36b2f8f8fc6f107e3e66f6bd62cee3222ee8ee.zip"
+  sha256 "3387758a93914a06e69b4e995cda65301ac9c5e6c2314ee5485b99e5b2a9a755"
+  version "2016.11.14.4" # <year>.<month>.<day>.<commit number that day>
 
   depends_on "cmake" => :build
   depends_on "zmq"
+  depends_on "asio"
 
   def install
     system "cmake", ".", *std_cmake_args
